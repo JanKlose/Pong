b1430522512b0bc1f48a289b43989841bc13056c
JanKlose <59050767+JanKlose@users.noreply.github.com>
2021-04-26 19:32:34 +0200

    Update Draw.java

--- src/pack1/Draw.java
+++ src/pack1/Draw.java
@@ -22,16 +22,17 @@ public class Draw extends JLabel {
             g.fillRect(Var.screenwidth/2 -5, i*20, 10, 10);
 
         }
+        //Player and Enemy bar
+        g.fillRect(Var.x, Var.y, 25, 150); 
+        g.fillRect(Var.ggnx, Var.ggny, 25, 150); 
 
-        g.fillRect(Var.x, Var.y, 25, 150);
-        g.fillRect(Var.ggnx, Var.ggny, 25, 150);
-
+        //counting the score
         g.drawString(""+ Var.playerPoints, Var.screenwidth/2-95, 75);
         g.drawString(""+ Var.ggnPoints, Var.screenwidth/2+50, 75);
 
         g.fillRect(Var.ballx, Var.bally, 20, 20);
 
-        repaint();
+        repaint(); // to visualize movement
 
         }
 
