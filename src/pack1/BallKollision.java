1dce5d238a81609db76ebb555eb58c62dcdea540
JanKlose <59050767+JanKlose@users.noreply.github.com>
2021-04-26 19:30:48 +0200

    Update BallKollision.java

--- src/pack1/BallKollision.java
+++ src/pack1/BallKollision.java
@@ -11,15 +11,15 @@ public class BallKollision {
         kollision.scheduleAtFixedRate(new TimerTask() {
             @Override
             public void run() {
-                // Kollision unten
+                // Collision bottom
                 if(Var.bally+50>= Var.screnheigth){
                     Var.ballDiry = -1;
                 }
-                // oben
+                // up
                 if(Var.bally <= 0){
                     Var.ballDiry = 1;
                 }
-                // rechts
+                // right
                 if (Var.ballx +20 >= Var.screenwidth){
                     Var.ballx = Var.screenwidth/2 -10;
                     Var.bally = Var.screnheigth/2 -10;
@@ -27,7 +27,7 @@ public class BallKollision {
                     Var.ballDirx = -1;
                     Var.playerPoints++;
                 }
-                // links
+                // left
                 if (Var.ballx <= 0){
                     Var.ballx = Var.screenwidth/2 - 10;
                     Var.bally = Var.screnheigth/2 -10;
@@ -39,7 +39,7 @@ public class BallKollision {
                 if(Var.ballx < Var.x+25 && Var.ballx > Var.x && Var.bally-20 < Var.y+150 && Var.bally> Var.y){
                     Var.ballDirx = 1;
                 }
-                //HitBox Gegner
+                //HitBox Enemy
                 if(Var.ballx < Var.ggnx && Var.ballx > Var.ggnx -25 && Var.bally-20 < Var.ggny+150 && Var.bally> Var.ggny){
                     Var.ballDirx = -1;
                 }
